26349dc09a07a45fa153c268883d3c056d74b3c8
Nuno Tomé <nunotomeit@gmail.com>
2024-03-09 12:26:42 +0000

    no message

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 from gradio_client import Client
 import gradio as gr
 
+DEBUG_MODE = True
+
 client = Client("Nuno-Tome/API_demo_server")
 
 def request(text):
+    if DEBUG_MODE:
+        print(f"Client: {client}")
+        print(f"Requesting prediction for: {text}")
     result = client.predict(
-		"Hello World",	# str  in 'text' Textbox component
+		#"Hello World",	# str  in 'text' Textbox component
+        text,
 		api_name="/predict"
-    ) 
+    )
+    if DEBUG_MODE:
+        print(f"Prediction result: {result}")
     return result
 
 
